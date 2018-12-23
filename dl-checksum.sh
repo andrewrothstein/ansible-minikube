d23a80c71d382dad545881b3d3527539e39bce61
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-12-23 15:46:11 -0500

    up to v0.32.0

---
+++ dl-checksum.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+VER=v0.32.0
+DIR=~/Downloads
+MIRROR=https://github.com/kubernetes/minikube/releases/download/$VER
+
+dl()
+{
+    OS=$1
+    PLATFORM=$2
+    wget -O $DIR/minikube-$VER-$OS-$PLATFORM $MIRROR/minikube-$OS-$PLATFORM
+}
+
+dl linux amd64
+dl darwin amd64
+dl windows amd64
+sha256sum $DIR/minikube-$VER-*
+
