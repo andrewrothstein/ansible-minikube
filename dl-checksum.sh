fb0b542f8c9b4729283ed27ea53f5d13a08bfb11
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-08-06 12:01:43 -0500

    up to v1.3.0. upgrade dep vers. cleanup dl-checksum.sh

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER=v1.2.0
+VER=v1.3.0
 DIR=~/Downloads
 MIRROR=https://github.com/kubernetes/minikube/releases/download/$VER
 
@@ -8,10 +8,10 @@ dl_minikube()
     OS=$1
     PLATFORM=$2
     SUFFIX=$3
-    OP="${OS}-${PLATFORM}${SUFFIX}"
+    OP=${OS}-${PLATFORM}${SUFFIX}
     URL=$MIRROR/minikube-${OP}.sha256
-    printf "  # ${URL}\n"
-    printf "  ${OS}-${PLATFORM}: sha256:%s\n" `curl -sSL $URL | awk '{print $1}'`
+    printf "    # %s\n" $URL
+    printf "    %s-%s: sha256:%s\n" $OS $PLATFORM `curl -sSL $URL | awk '{print $1}'`
 }
 
 dl_driver()
@@ -19,12 +19,12 @@ dl_driver()
     DRIVER_NAME=$1
     FILE_NAME=docker-machine-driver-$DRIVER_NAME.sha256
     URL=$MIRROR/$FILE_NAME
-    printf "minikube_driver_${DRIVER_NAME}_checksums:\n"
-    printf "  # ${URL}\n"
-    printf "  $VER: sha256:%s\n" `curl -sSL $URL | awk '{print $1}'`
+    printf "minikube_driver_%s_checksums:\n" $DRIVER_NAME
+    printf "  # %s\n" $URL
+    printf "  %s: sha256:%s\n" $VER `curl -sSL $URL | awk '{print $1}'`
 }
 
-printf "$VER:\n"
+printf "  %s:\n" $VER
 dl_minikube linux amd64
 dl_minikube darwin amd64
 dl_minikube windows amd64 .exe
