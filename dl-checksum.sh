a49d2a7f4e473e4bf7f804dbbb84873352a59e23
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-12-18 08:26:09 -0500

    up to v1.6.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,34 +1,42 @@
 #!/usr/bin/env sh
-VER=${1:-v1.5.2}
 DIR=~/Downloads
-MIRROR=https://github.com/kubernetes/minikube/releases/download/$VER
+MIRROR=https://github.com/kubernetes/minikube/releases/download
 
 dl_minikube()
 {
-    local os=$1
-    local arch=$2
-    local suffix=$3
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local suffix=$4
     local platform=${os}-${arch}
     local qsuffix=${platform}${suffix}
-    local url=$MIRROR/minikube-${qsuffix}.sha256
+    local url=$MIRROR/$ver/minikube-${qsuffix}.sha256
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform `curl -sSL $url | awk '{print $1}'`
 }
 
 dl_driver()
 {
-    local driver_name=$1
+    local ver=$1
+    local driver_name=$2
     local file_name=docker-machine-driver-$driver_name.sha256
-    local url=$MIRROR/$file_name
+    local url=$MIRROR/$ver/$file_name
     printf "minikube_driver_%s_checksums:\n" $driver_name
     printf "  # %s\n" $url
-    printf "  %s: sha256:%s\n" $VER `curl -sSL $url | awk '{print $1}'`
+    printf "  %s: sha256:%s\n" $ver `curl -sSL $url | awk '{print $1}'`
 }
 
-printf "  %s:\n" $VER
-dl_minikube linux amd64
-dl_minikube darwin amd64
-dl_minikube windows amd64 .exe
+dl_ver() {
+    local ver=$1
 
-dl_driver kvm2
-dl_driver hyperkit
+    printf "  %s:\n" $ver
+    dl_minikube $ver linux amd64
+    dl_minikube $ver darwin amd64
+    dl_minikube $ver windows amd64 .exe
+
+    dl_driver $ver kvm2
+    dl_driver $ver hyperkit
+}
+
+dl_ver v1.6.0
+dl_ver ${1:-v1.6.1}
