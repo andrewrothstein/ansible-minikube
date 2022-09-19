db6eefea047401a53e65d38fa6069d6fe6c160af
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-09-19 15:53:37 -0400

    minikube:1.27.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -30,12 +30,17 @@ dl_ver() {
 
     printf "  %s:\n" $ver
     dl_minikube $ver linux amd64
+    dl_minikube $ver linux arm
+    dl_minikube $ver linux arm64
+    dl_minikube $ver linux ppc64le
+    dl_minikube $ver linux s390x
     dl_minikube $ver darwin amd64
+    dl_minikube $ver darwin arm64
     dl_minikube $ver windows amd64 .exe
 
 }
 
-VER=${1:-v1.26.1}
+VER=${1:-v1.27.0}
 dl_ver $VER >> defaults/main/minikube_checksums.yml
 dl_driver $VER kvm2 >> defaults/main/minikube_driver_kvm2_checksums.yml
 dl_driver $VER hyperkit >> defaults/main/minikube_driver_hyperkit_checksums.yml
